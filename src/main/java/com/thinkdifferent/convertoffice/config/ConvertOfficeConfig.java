14d240a3320f0cfb0c653cf7b05f4d9e7655b338
zhangzhen1979 <zhen.zhang@163.com>
2021-12-22 16:12:33 +0800

    加入Nacos动态刷新注解。

--- src/main/java/com/thinkdifferent/convertoffice/config/ConvertOfficeConfig.java
+++ src/main/java/com/thinkdifferent/convertoffice/config/ConvertOfficeConfig.java
@@ -1,8 +1,10 @@
 package com.thinkdifferent.convertoffice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
+@RefreshScope
 @Configuration
 public class ConvertOfficeConfig {
 
