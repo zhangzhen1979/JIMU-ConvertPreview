a50c88fd742bdfe69d3137a433a8c11063dc715f
zhangzhen1979 <zhen.zhang@163.com>
2021-12-22 16:28:18 +0800

    修正注解遗漏。

--- src/main/java/com/thinkdifferent/convertoffice/ConvertofficeApplication.java
+++ src/main/java/com/thinkdifferent/convertoffice/ConvertofficeApplication.java
@@ -3,9 +3,11 @@ package com.thinkdifferent.convertoffice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOpenApi
 public class ConvertofficeApplication {
 
     public static void main(String[] args) {
